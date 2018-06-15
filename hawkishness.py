2807f82c7248405ba95156a2244eb182ebefddb9
Dmitry Brizhinev <dmitry.brizhinev@anu.edu.au>
2018-06-15 17:31:41 +1000

    Added videos

--- hawkishness.py
+++ hawkishness.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 def utility(c, r):
     if r == 1:
@@ -14,23 +15,53 @@ def inverse_utility(u, r):
         return ((u * (1-r)) + 1)**(1/(1-r)) - 1
 
 def expect(pWin, h, r):
-    return inverse_utility(pWin * utility(2 + h, r) + (1-pWin) * utility(1 - h, r), r) - 1
+    return inverse_utility(pWin * utility(2 + h, r) + (1-pWin) * utility(1 + h, r), r) - 1
 
 def deal(pWin, hA, hB, rA, rB):
     wantA = expect(pWin, hA, rA)
     wantB = 1 - expect(1-pWin, hB, rB)
-    print(wantA, wantB)
-    return wantA <= wantB
+    return wantB - wantA
 
-def diagram():
-    pWin = 0.5
-    rA = 0.1
-    rB = 0.1
-    hA = np.array([[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]]*10)
-    hB = np.array([[0]*10, [0.1]*10, [0.2]*10, [0.3]*10, [0.4]*10, [0.5]*10, [0.6]*10, [0.7]*10, [0.8]*10, [0.9]*10])
-    print(hA, hB)
-    print(deal(pWin, hA, hB, rA, rB))
+    
+hMin = -1
+hMax = 1
+def diagram(pWin, r, ax):
+    pWin = pWin
+    rA = r
+    rB = r
+    hAs = np.linspace(hMin, hMax, 100)
+    hBs = np.linspace(hMin, hMax, 100)
+    hA, hB = np.meshgrid(hAs, hBs)
+    im = ax.imshow(deal(pWin, hA, hB, rA, rB), cmap="RdYlGn", vmin=-1, vmax=1, extent=[hMin,hMax,hMin,hMax], origin='lower')
+    plt.colorbar(im)
+    return ax
+    
+def update_power(t, ax, fig, r, total):
+    pWin = t/total
+    fig.clear()
+    ax = plt.gca()
+    fig.suptitle('pWin: {}'.format(pWin))
+    return diagram(pWin, r, ax)
+    
+def update_risk_aversion(t, ax, fig, pWin, total):
+    r = (t/total)*2 - 1
+    fig.clear()
+    ax = plt.gca()
+    fig.suptitle('Risk Aversion: {}'.format(r))
+    return diagram(pWin, r, ax)
+    
+def video_power(r):
+    fig, ax = plt.gcf(), plt.gca()
+    ani = animation.FuncAnimation(fig, update_power, 101, fargs=(ax, fig, r, 100), interval=10, blit=False, repeat=False)
+    plt.show()
+    plt.close()
+    
+def video_risk_aversion(pWin):
+    fig, ax = plt.gcf(), plt.gca()
+    ani = animation.FuncAnimation(fig, update_risk_aversion, 101, fargs=(ax, fig, pWin, 100), interval=10, blit=False, repeat=False)
+    plt.show()
+    plt.close()
     
 if __name__ == "__main__":
-    diagram()
+    video_power(5)
     
