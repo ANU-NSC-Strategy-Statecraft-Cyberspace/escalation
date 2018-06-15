1ffeed9423c7eaaf50fad7260c66136ca8ec5bb3
Dmitry Brizhinev <dmitry.brizhinev@anu.edu.au>
2018-06-15 14:59:16 +1000

    New hawkishness model

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Escalation
+# Hawkishness
 
-Model of escalation from a cyber to a kinetic conflict.
+Model of the effects of hawkishness and risk aversion on conflict.
 
 Created by Dmitry Brizhinev <dmitry.brizhinev@anu.edu.au> for the Strategy and Statecraft in Cyberspace project,
 National Security College <http://nsc.anu.edu.au/>, Australian National University <http://anu.edu.au/>
--- hawkishness.py
+++ hawkishness.py
@@ -1,74 +1,36 @@
-from random import random, choice, sample
-from statistics import median
-from itertools import combinations
-import csv
 import numpy as np
-import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-from cycler import cycler
-from functools import lru_cache as memoize
-import colorsys
-import itertools
 
-def p_win(pA, pB):
-    return ( pA / (pA + pB), pB / (pA + pB) )
-'''
-epsilon = 1e-15
-def utility(c, a):
-    interim = -np.exp(-c*a) / a
-    interim[~np.isfinite(interim)] = c
-    return interim
-'''
-
-from scipy.stats import norm
-def eval(riskAs, riskBs, hawkAs, hawkBs, powerAs, powerBs):
-    war_a = hawkAs * norm.cdf(powerAs * np.exp(-riskAs), loc=powerBs,scale=powerBs)
-    war_b = hawkBs * norm.cdf(powerBs * np.exp(-riskBs), loc=powerAs,scale=powerAs)
-    return 1 - (1 - war_a)*(1 - war_b)
-
-prec=0.02
-
-def color(war_prob):
-    #color = (not_peace, not_peace - war, 1.0 - not_peace)
-    #color = (not_peace, 1.0 - war, 0)
-    return np.array([war_prob, 1.0 - war_prob, 0])
-
-
-def draw(plot=True, read=False, save=False, file='data.csv', powera=1, powerb=1, vriska=True, vriskb=False, vhawka=True, vhawkb=False):
-    assert not save and not read
-    powera = np.array([powera])
-    powerb = np.array([powerb])
-    assert sum([vriska, vriskb, vhawka, vhawkb]) == 2
-    labels = np.array(["Country 1 Risk Aversion", "Country 2 Risk Aversion", "Country 1 Hawkishness", "Country 2 Hawkishness"])[[vriska, vriskb, vhawka, vhawkb]]
-    assert len(labels) == 2
-    riska = np.arange(0.0,1.0 + prec,prec) if vriska else np.array([0])
-    riskb = np.arange(0.0,1.0 + prec,prec) if vriskb else np.array([0])
-    hawka = np.arange(0.0,1.0 + prec,prec) if vhawka else np.array([1])
-    hawkb = np.arange(0.0,1.0 + prec,prec) if vhawkb else np.array([1])
-
-    if read:
-        pass
-    #    X = np.loadtxt(file, delimiter=',')
+def utility(c, r):
+    if r == 1:
+        return np.log(c+1)
     else:
-        riskAs, riskBs, hawkAs, hawkBs, powerAs, powerBs = np.meshgrid(riska, riskb, hawka, hawkb, powera, powerb, indexing='ij')
-        X = eval(riskAs, riskBs, hawkAs, hawkBs, powerAs, powerBs)
-        #if save:
-        #    np.savetxt(file, X, fmt='%.3f', delimiter=',')
-    if plot:
-        index = (slice(None) if vriska else 0, slice(None) if vriskb else 0, slice(None) if vhawka else 0, slice(None) if vhawkb else 0, 0,0)
-        Y = np.vectorize(color, signature='()->(n)')(X[index])
-        plt.close()
-        fig, ax = plt.subplots()
-        ax.imshow(Y, origin='lower', extent=(0.0, 1.0, 0.0, 1.0))
-        ax.set_xlabel(labels[0], size=14)
-        ax.set_ylabel(labels[1], size=14)
-        ax.text(-0.02,0,'Low', size=12, horizontalalignment='right', verticalalignment='bottom', transform=ax.transAxes)
-        ax.text(-0.02,1,'High', size=12, horizontalalignment='right', verticalalignment='top', transform=ax.transAxes)
-        ax.text(0,-0.02,'Low', size=12, horizontalalignment='left', verticalalignment='top', transform=ax.transAxes)
-        ax.text(1,-0.02,'High', size=12, horizontalalignment='right', verticalalignment='top', transform=ax.transAxes)
-        ax.tick_params(bottom=False, left=False, top=False, right=False, labelbottom=False, labelleft=False)
-        plt.subplots_adjust(left=0.06, bottom=0.06, right=0.76, top=0.98)
-        #plt.legend(handles=[mpatches.Patch(color='red', label='War'), mpatches.Patch(color='gold', label='Standoff'), mpatches.Patch(color='limegreen', label='Peace')], loc=(1.05, 0.8))
-        plt.legend(handles=[mpatches.Patch(color='red', label='War'), mpatches.Patch(color='limegreen', label='Peace')], loc=(1.05, 0.8))
-        plt.show()
+        return ((c+1)**(1-r) - 1)/(1-r)
+        
+def inverse_utility(u, r):
+    if r == 1:
+        return np.exp(u) - 1
+    else:
+        return ((u * (1-r)) + 1)**(1/(1-r)) - 1
+
+def expect(pWin, h, r):
+    return inverse_utility(pWin * utility(2 + h, r) + (1-pWin) * utility(1 - h, r), r) - 1
+
+def deal(pWin, hA, hB, rA, rB):
+    wantA = expect(pWin, hA, rA)
+    wantB = 1 - expect(1-pWin, hB, rB)
+    print(wantA, wantB)
+    return wantA <= wantB
+
+def diagram():
+    pWin = 0.5
+    rA = 0.1
+    rB = 0.1
+    hA = np.array([[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]]*10)
+    hB = np.array([[0]*10, [0.1]*10, [0.2]*10, [0.3]*10, [0.4]*10, [0.5]*10, [0.6]*10, [0.7]*10, [0.8]*10, [0.9]*10])
+    print(hA, hB)
+    print(deal(pWin, hA, hB, rA, rB))
+    
+if __name__ == "__main__":
+    diagram()
+    
